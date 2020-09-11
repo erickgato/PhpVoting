723d878f124e59fd4047c30a0b5c57a08c7e3f28
erickgato <itsbk0703@gmail.com>
2020-09-11 14:28:27 -0300

    Env debug setted to true

--- config/env.php
+++ config/env.php
@@ -2,8 +2,9 @@
 //Database
 $_ENV['DB']['name'] = 'phpvotingdatabase';
 $_ENV['DB']['user'] = 'root';
-$_ENV['DB']['password'] = '3306';
-$_ENV['DB']['instance'] = 'localhost';
+$_ENV['DB']['password'] = '';
+$_ENV['DB']['port'] = '3306';
+$_ENV['DB']['instance'] = '127.0.0.1';
 
 $_ENV['DEBUG'] = false;
 
