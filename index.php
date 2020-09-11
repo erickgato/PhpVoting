38ae638d8be4a7543b5e36f89c3178319603df87
erickgato <itsbk0703@gmail.com>
2020-09-11 14:28:48 -0300

    Select users test

--- index.php
+++ index.php
@@ -2,5 +2,10 @@
 // Include the config file to setup env variables
 include_once 'config/env.php';
 require __DIR__ . '/vendor/autoload.php';
+//Set UTF-8 as global encode
+use App\Model\User as User;
 
-var_dump($_ENV);
+$usr = new User();
+
+var_dump($usr->SELECT('*'));
+mb_internal_encoding('UTF-8');
