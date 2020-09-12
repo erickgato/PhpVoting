b503998cc876fa9e6a2e192f35efe9688fc2b500
erickgato <itsbk0703@gmail.com>
2020-09-12 09:01:01 -0300

    Route for project

--- index.php
+++ index.php
@@ -3,12 +3,31 @@
 include_once 'config/env.php';
 require __DIR__ . '/vendor/autoload.php';
 //Set UTF-8 as global encode
-use App\Model\User;
-use App\Model\Status;
-use App\Model\Imodel;
-use App\Model\Survey;
-use App\Model\Generics\Database;
-
 mb_internal_encoding('UTF-8');
 
-require_once './src/public/index.php';
+use CoffeeCode\Router\Router;
+
+$router = new Router(PJURL);
+$router->namespace("App\Controllers");
+
+$router->group(null);
+$router->get("/", "Login:index");
+$router->post("/", "Login:receiveData");
+
+$router->group("cadastro");
+$router->get("/", "SingUp:index");
+$router->post("/", "SingUp:receiveData");
+
+$router->group("explorar");
+$router->get("/", "App:explore");
+$router->get("/enquete/{id}", "App:survey");
+
+/** 
+ * error group
+ */
+$router->group("oops");
+$router->get("/{errcode}", "Error:trow");
+
+$router->dispatch();
+if ($router->error())
+    $router->redirect("/oops/{$router->error()}");
