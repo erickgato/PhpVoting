f3e074a1fe034bce7b9e55652c7f63ccbe04d0c9
erickgato <itsbk0703@gmail.com>
2020-09-11 22:08:17 -0300

    Added dependencing control

--- src/model/user.model.php
+++ src/model/user.model.php
@@ -1,28 +1,32 @@
-<?php 
+<?php
+
 namespace App\Model;
 
-use App\Model\Actions as Actions;
-use App\Model\Table\Table as Table;
-use App\Model\Table\PrimaryIndex;
-use App\Model\Database as Database;
+use App\Model\Generics\Actions;
+use App\Model\Generics\Table;
+use App\Model\Generics\PrimaryIndex;
+use App\Model\Generics\Database;
+use App\Model\Imodel;
 
 /**
  * @author Erick gato <a.itsbk0703@gmail.com>
- * @description User model for keep CRUD into controllers
+ * @description User model
  * 
  */
-class User extends Actions {
+class User extends Actions implements Imodel
+{
     private Database $databaseInstance;
 
-    public function __construct() {
-        $this->databaseInstance = new Database() ;
+    public function __construct(Database $DBinstance)
+    {
+        $this->databaseInstance = $DBinstance;
         $this->Mysqlconection = $this->databaseInstance->UP();
-        $this->_Table = new Table('en_user',["usr_name","usr_email","usr_pass"],new PrimaryIndex('usr_id',0));
+        $this->_Table = new Table('en_user', ["usr_name", "usr_email", "usr_pass"], new PrimaryIndex('usr_id', 0));
     }
 
-    public function __destruct()
+    public function DestroyConection()
     {
         $this->Mysqlconection->close();
+        $this->databaseInstance->DOWN();   
     }
-    
 }
