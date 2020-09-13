32e980a2fe0da4740b84ad2f4c2c4d2252308d20
erickgato <itsbk0703@gmail.com>
2020-09-13 00:28:51 -0300

    Page components

---
+++ src/public/pages/components/menu.php
@@ -0,0 +1,20 @@
+<nav class="menu">
+    <ul>
+        <li> <a href="cadastro"> Cadastrar </a></li>
+        <li><a href="<? echo PJURL ?>/enquetes/"> Navegar </a></li>
+    </ul>
+</nav>
+
+<nav class="mobile" role="navigation">
+    <div id="menuToggle">
+        <input type="checkbox" />
+        <span></span>
+        <span></span>
+        <span></span>
+        <ul id="menu">
+            <li><a href="cadastro"> Cadastrar </a></li>
+            <li><a href="<? echo PJURL  ?>/enquetes/"> Navegar </a></li>
+
+        </ul>
+    </div>
+</nav>
