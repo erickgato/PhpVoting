f7d3c45670940ad55fcb369bede6ccaa0214dd4a
erickgato <itsbk0703@gmail.com>
2020-09-13 23:26:21 -0300

    Register survey datetime corretion

--- src/public/pages/cadastrar_enquete.php
+++ src/public/pages/cadastrar_enquete.php
@@ -40,7 +40,6 @@
             <button type="submit"> Enviar </button>
         </form>
     </div>
-    <script src="src/public/js/index.js"></script>
     <script>
         window.addEventListener('load', function() {
             const _plusButton = document.getElementById('plus_options');
